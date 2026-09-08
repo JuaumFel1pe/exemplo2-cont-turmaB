ab0cda1b2f27005f83b8e3d88d2ab003531ab584
João Felipe de Melo Nascimento <129508479+JuaumFel1pe@users.noreply.github.com>
2026-09-08 13:49:47 +0000

    Finalização do CRUD básico

--- src/main/java/br/senac/tads/dsw/exemplo2/controller/ProdutoController.java
+++ src/main/java/br/senac/tads/dsw/exemplo2/controller/ProdutoController.java
@@ -1,9 +1,15 @@
 package br.senac.tads.dsw.exemplo2.controller;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,4 +45,66 @@ public class ProdutoController {
         return ResponseEntity.created(location).body(produtoSalvo);
     }
     //por padrão, assim que você faz um post ele retorna o json da modificação feita
+
+        //Endpoint get para listar todos os produtos
+    @GetMapping 
+    public List<Produto>listarTodos(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")                      //PathVariable serve para ele saber que está recebendo o id junto com o caminho
+    public ResponseEntity<Produto>buscarPorId(@PathVariable  Long id){
+        
+        //serve para ser opcional, pra caso receber um valor nulo, ele não dar erro.
+        //Caso o produto não exista, não dará erro 
+        Optional<Produto> produtoBuscado = repository.findById(id);
+
+        //isPresent para ver se realmente existe algo
+        if(produtoBuscado.isPresent()){
+            //se existir ele retornará um ok, retornando um get com o produto especificado
+            return ResponseEntity.ok(produtoBuscado.get());
+        }
+        else{
+            //Informará que não foi encontrada (build serve apenas para construir a estrutura da resposta)
+            return ResponseEntity.notFound().build();
+        }
+        
+    }
+
+    @PutMapping("/{id}")//Recebe o id porque vai atualizar um produto em especifico
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody Produto produtoAtualizado){
+        //Optional porque o produto pode não existir
+        Optional<Produto> produtoBuscado = repository.findById(id);
+
+        if(produtoBuscado.isPresent()){
+            Produto produtoExistente = produtoBuscado.get();
+
+            //Sobrepondo os nomes e preços do produto existente para os novos dados
+            produtoExistente.setNome(produtoAtualizado.getNome());
+            produtoExistente.setPreco(produtoAtualizado.getPreco());
+
+            Produto produtoSalvo = repository.save(produtoExistente);
+
+            return ResponseEntity.ok(produtoSalvo);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> apagarProduto(@PathVariable Long id){
+        Optional<Produto> produtoBuscado = repository.findById(id);
+
+        if(produtoBuscado.isPresent()){
+            repository.deleteById(id);
+
+            //Usa o noContent porque não precisa retornar nada, já que ele apaga o produto
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
 }
